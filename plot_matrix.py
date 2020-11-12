2059a734f84762dfd73578150fbb0a06c61dc6a3
tingsong <tingsong@pku.edu.cn>
2020-11-12 16:27:37 +0800

    add matplotlib.image.imsave()
    test

--- plot_matrix.py
+++ plot_matrix.py
@@ -1,14 +1,17 @@
 # TODO: import ...
-
-
+import numpy as np
+#import matplotlib
+#import matplotlib.pylot as plt
+import matplotlib.image as plt2
 def generate_random_matrix(m, n):
-    raise NotImplementedError  # TODO: 删除该行，实现该函数功能
-
-
+    matrix = np.random.randint(0, 2, size=(m,n))#numpy.random.randint(low, high=None, size=None, dtype='l')
+    return matrix
 def save_matrix(matrix, file_name):
-    raise NotImplementedError  # TODO: 删除该行，实现该函数功能
-
+    #plt.imshow(matrix)
+    #plt.savefig(file_name)
+    #matplotlib.image.imsave(file_name, matrix)
+    plt2.imsave(file_name, matrix)
 
 if __name__ == "__main__":
     matrix = generate_random_matrix(10, 10)
-    save_matrix(matrix, "example.jpg")
+    save_matrix(matrix, "example.png")
