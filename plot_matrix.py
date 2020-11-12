12e84e8664e1743efbe1441c774e8ad51ff5401d
tingsong <tingsong@pku.edu.cn>
2020-11-12 16:39:00 +0800

    flake8 规范
    flake8要求文件每一行都以换行结束，只需要在文件结尾回车即可（最后一行必须是空行，有多余的空格还会报错）。

--- plot_matrix.py
+++ plot_matrix.py
@@ -2,10 +2,11 @@
 import numpy as np
 import matplotlib.image as plt2
 def generate_random_matrix(m, n):
-    matrix = np.random.randint(0, 2, size=(m,n))#numpy.random.randint(low, high=None, size=None, dtype='l')
+    matrix = np.random.randint(0, 2, size=(m,n))
     return matrix
 def save_matrix(matrix, file_name):
     plt2.imsave(file_name, matrix)
 if __name__ == "__main__":
     matrix = generate_random_matrix(10, 10)
     save_matrix(matrix, "example.jpg")
+    
